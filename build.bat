e0f068a36a604421d4d51b4b73b8d0ea554cb4fb
honeytidy <honeytidy@gmail.com>
2025-02-28 21:58:29 +0800

    add copy script

--- build.bat
+++ build.bat
@@ -7,15 +7,15 @@ if exist dist rd /s /q dist
 if exist main.spec del /q main.spec
 
 :: 检查是否具有管理员权限
-rem net session >nul 2>&1
-rem if %errorlevel% neq 0 (
-rem     echo 当前脚本没有管理员权限，正在尝试以管理员权限重新启动...
-rem     powershell -Command "Start-Process -FilePath '%~f0' -Verb RunAs"
-rem     rem exit /b
-rem )
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo 当前脚本没有管理员权限，正在尝试以管理员权限重新启动...
+    powershell -Command "Start-Process -FilePath '%~f0' -Verb RunAs"
+    exit /b
+)
 
 :: 调用 PyInstaller
-pyinstaller -i aitool.ico -w --add-data "aitool.ico;." main.py
+pyinstaller -i aitool.ico -w --add-data "aitool.ico;." --clean main.py
 xcopy /q "setup.bat" "dist/main"
 echo 打包完成！
 
